db144f991955bafc39b250af4ee04480f44eda15
Polymathh <wambugum110@gmail.com>
2024-12-30 17:54:14 +0300

    initial commit

--- mdogo/portfolio/urls.py
+++ mdogo/portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import logout_view
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('cartoon/<int:pk>/', views.cartoon_detail, name='cartoon_detail'),
-    
+    path('logout/', logout_view, name='logout'),
 ]
--- mdogo/portfolio/views.py
+++ mdogo/portfolio/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cartoon
-
+from django.contrib.auth import logout
+from django.urls import path
 
 # Create your views here.
 def home(request):
@@ -11,3 +12,7 @@ def home(request):
 def cartoon_detail(request, pk):
     cartoon = get_object_or_404(Cartoon, pk=pk)
     return render(request, 'cartoon_detail.html', {'cartoon': cartoon})
+
+def logout_view(request):
+    logout(request)  # Logs out the user
+    return redirect('home')  

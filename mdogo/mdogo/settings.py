71bb7630f5e128a45e526e1dfc3f6df0f291ff44
Polymathh <wambugum110@gmail.com>
2025-01-16 16:10:32 +0300

    initial commit

--- mdogo/mdogo/settings.py
+++ mdogo/mdogo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tk_$ip9%#@f37!2kgp^(1s2q!zvvi!h#31l9=10ym40t9$ng6g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
